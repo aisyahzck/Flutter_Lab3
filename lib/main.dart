26da2320a718775626322d0f579249afa0b366bb
aisyahzck <74127545+aisyahzck@users.noreply.github.com>
2021-04-13 18:22:52 +0800

    Code update

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('My Quiz'),
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
